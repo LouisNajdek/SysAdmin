258fd40382c7524604e73246cee3334adc5fde7a
LouisNajdek <louis@web01-louis.louis.local>
2020-02-27 00:00:53 -0500

    Script

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#secure-ssh.sh
+#Author: Louis Najdek
+#Creates a new ssh user with $1 paramater
+#Adds a public key from the local repo or curled from the remote repo
+echo "User: [$1]"
+useradd -m -d /home/$1 -s /bin/bash $1
+mkdir /home/$1/.ssh
+cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1 /home/$1/.ssh
+
