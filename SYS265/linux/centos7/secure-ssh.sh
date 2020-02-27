4b7484e6df423f662ade34dfa0da7bbf2525a36f
LouisNajdek <louis@web01-louis.louis.local>
2020-02-27 00:10:06 -0500

    Script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -6,7 +6,7 @@
 echo "User: [$1]"
 useradd -m -d /home/$1 -s /bin/bash $1
 mkdir /home/$1/.ssh
-cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+cp ../public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
 chmod 700 /home/$1/.ssh
 chmod 600 /home/$1/.ssh/authorized_keys
 chown -R $1:$1 /home/$1/.ssh
